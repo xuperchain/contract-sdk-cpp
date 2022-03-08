d2cba4efaa8aa6cf80253eeed965b6a570fe05b6
chenfengjin <1871653365@qq.com>
2022-03-08 15:22:35 +0800

    bugfix:global variable initialize failed in example/short_content

--- example/short_content.cc
+++ example/short_content.cc
@@ -1,6 +1,6 @@
 #include "xchain/xchain.h"
 
-const std::string UserBucket = "USER";
+const char* UserBucket = "USER";
 const int TOPIC_LENGTH_LIMIT = 36;
 const int TITLE_LENGTH_LIMIT = 100;
 const int CONTENT_LENGTH_LIMIT = 3000;
@@ -21,7 +21,8 @@ public:
     virtual void queryByTopic() = 0;
 };
 
-struct ShortContentDeposit : public ShortContentDepositBasic, public xchain::Contract {
+struct ShortContentDeposit : public ShortContentDepositBasic,
+                             public xchain::Contract {
 public:
     void initialize() {
         xchain::Context* ctx = this->context();
@@ -33,10 +34,14 @@ public:
         std::string title = ctx->arg("title");
         std::string topic = ctx->arg("topic");
         std::string content = ctx->arg("content");
-        const std::string userKey = UserBucket + "/" + user_id + "/" + topic + "/" + title;
-        if (topic.length() > TOPIC_LENGTH_LIMIT || title.length() > TITLE_LENGTH_LIMIT ||
+        const std::string userKey =
+            std::string(UserBucket) + "/" + user_id + "/" + topic + "/" + title;
+        if (topic.length() > TOPIC_LENGTH_LIMIT ||
+            title.length() > TITLE_LENGTH_LIMIT ||
             content.length() > CONTENT_LENGTH_LIMIT) {
-            ctx->error("The length of topic or title or content is more than limitation");
+            ctx->error(
+                "The length of topic or title or content is more than "
+                "limitation");
             return;
         }
         if (ctx->put_object(userKey, content)) {
@@ -47,8 +52,10 @@ public:
     }
     void queryByUser() {
         xchain::Context* ctx = this->context();
-        std::string key = UserBucket + "/" + ctx->arg("user_id") + "/";
-        std::unique_ptr<xchain::Iterator> iter = ctx->new_iterator(key, key + "～");
+        std::string key =
+            std::string(UserBucket) + "/" + ctx->arg("user_id") + "/";
+        std::unique_ptr<xchain::Iterator> iter =
+            ctx->new_iterator(key, key + "～");
         std::string result;
         while (iter->next()) {
             std::pair<std::string, std::string> res;
@@ -62,7 +69,8 @@ public:
         std::string user_id = ctx->arg("user_id");
         std::string topic = ctx->arg("topic");
         std::string title = ctx->arg("title");
-        std::string key = UserBucket + "/" + user_id + "/" + topic + "/" + title;
+        std::string key =
+            std::string(UserBucket) + "/" + user_id + "/" + topic + "/" + title;
         std::string value;
         bool ret = ctx->get_object(key, &value);
         if (ret) {
@@ -75,9 +83,11 @@ public:
         xchain::Context* ctx = this->context();
         std::string user_id = ctx->arg("user_id");
         std::string topic = ctx->arg("topic");
-        std::string key = UserBucket + "/" + user_id + "/" + topic + "/";
+        std::string key =
+            std::string(UserBucket) + "/" + user_id + "/" + topic + "/";
         std::string result;
-        std::unique_ptr<xchain::Iterator> iter = ctx->new_iterator(key, key + "～");
+        std::unique_ptr<xchain::Iterator> iter =
+            ctx->new_iterator(key, key + "~");
         while (iter->next()) {
             std::pair<std::string, std::string> res;
             iter->get(&res);
@@ -87,22 +97,14 @@ public:
     }
 };
 
-DEFINE_METHOD(ShortContentDeposit, initialize) {
-    self.initialize();
-}
+DEFINE_METHOD(ShortContentDeposit, initialize) { self.initialize(); }
 
 DEFINE_METHOD(ShortContentDeposit, storeShortContent) {
     self.storeShortContent();
 }
 
-DEFINE_METHOD(ShortContentDeposit, queryByUser) {
-    self.queryByUser();
-}
+DEFINE_METHOD(ShortContentDeposit, queryByUser) { self.queryByUser(); }
 
-DEFINE_METHOD(ShortContentDeposit, queryByTitle) {
-    self.queryByTitle();
-}
+DEFINE_METHOD(ShortContentDeposit, queryByTitle) { self.queryByTitle(); }
 
-DEFINE_METHOD(ShortContentDeposit, queryByTopic) {
-    self.queryByTopic();
-}
+DEFINE_METHOD(ShortContentDeposit, queryByTopic) { self.queryByTopic(); }
