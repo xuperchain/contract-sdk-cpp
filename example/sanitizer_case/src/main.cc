cc567cd369f6a756a2a74d4eeb7550c3e210d6f5
FengjinChen <19762931+chenfengjin@users.noreply.github.com>
2022-05-13 11:27:44 +0800

    add counterexample for sanitizer (#40)

---
+++ example/sanitizer_case/src/main.cc
@@ -0,0 +1,29 @@
+#include "xchain/xchain.h"
+
+struct SanitizerCase : public xchain::Contract {};
+
+DEFINE_METHOD(SanitizerCase, initialize) {
+    xchain::Context* ctx = self.context();
+    ctx->ok("initialize succeed");
+
+}
+//  undefined_behavior is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, undefined_behavior) {
+    xchain::Context* ctx = self.context();
+    int *a = 0, b;
+    b = *a;
+    ctx->ok("ok");
+}
+
+//  buffer_overflow is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, buffer_overflow) {
+    xchain::Context* ctx = self.context();
+    int a[10];
+    int b = 10;
+    int c = a[b];
+    ctx->ok("ok");
+}
---
+++ example/sanitizer_case/test/sanitizer_case.test.js
@@ -0,0 +1,36 @@
+
+var assert = require("assert");
+
+Test("undefined_behavior", function (t) {
+    var contract;
+    t.Run("deploy", function (tt) {
+        contract = xchain.Deploy({
+            name: "sanitizer_case",
+            code: "../sanitizer_case.wasm",
+            lang: "c",
+            init_args: {},
+            options: { "account": "XC1111111111111111@xuper" }
+        })
+    });
+
+    t.Run("invoke", function (tt) {
+        resp = contract.Invoke("undefined_behavior", {});
+    })
+})
+
+Test("undefined_behavior", function (t) {
+    var contract;
+    t.Run("deploy", function (tt) {
+        contract = xchain.Deploy({
+            name: "sanitizer_case",
+            code: "../sanitizer_case.wasm",
+            lang: "c",
+            init_args: {},
+            options: { "account": "XC1111111111111111@xuper" }
+        })
+    });
+
+    t.Run("invoke", function (tt) {
+        resp = contract.Invoke("buffer_overflow", {});
+    })
+})
