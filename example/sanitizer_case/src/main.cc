962da9f796ac4bbd8bf70a4066328ee06ea45a59
FengjinChen <19762931+chenfengjin@users.noreply.github.com>
2022-05-20 17:16:24 +0800

    add more counterexample for sanitizer (#41)

--- example/sanitizer_case/src/main.cc
+++ example/sanitizer_case/src/main.cc
@@ -7,13 +7,25 @@ DEFINE_METHOD(SanitizerCase, initialize) {
     ctx->ok("initialize succeed");
 
 }
-//  undefined_behavior is a counterexample of cpp programing practice
+
+//  null_dereference is a counterexample of cpp programing practice
 //  which may cause things out of exception
 //  do not use it in your code
-DEFINE_METHOD(SanitizerCase, undefined_behavior) {
+DEFINE_METHOD(SanitizerCase, null_dereference) {
     xchain::Context* ctx = self.context();
     int *a = 0, b;
     b = *a;
+    ctx->ok("ok");
+}
+
+//  double_delete is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, double_delete) {
+    xchain::Context* ctx = self.context();
+     auto a  = new std::string();
+	delete(a);
+	delete(a);
     ctx->ok("ok");
 }
 
@@ -25,5 +37,36 @@ DEFINE_METHOD(SanitizerCase, buffer_overflow) {
     int a[10];
     int b = 10;
     int c = a[b];
+    ctx->ok("ok");
+}
+
+//  int_overflow is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, int_overflow) {
+    xchain::Context* ctx = self.context();
+    int a = 2147483647;
+    int b = a + 1;
+    ctx->ok("ok");
+}
+
+//  divide_by_zero is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, divide_by_zero) {
+    xchain::Context* ctx = self.context();
+    int a = 2147483647;
+    int b = 0;
+    int c = a / b;
+    ctx->ok("ok");
+}
+
+//  float_cast_overflow is a counterexample of cpp programing practice
+//  which may cause things out of exception
+//  do not use it in your code
+DEFINE_METHOD(SanitizerCase, float_cast_overflow) {
+    xchain::Context* ctx = self.context();
+    double a = 2147483699.1;
+    int b = int(a);
     ctx->ok("ok");
 }
